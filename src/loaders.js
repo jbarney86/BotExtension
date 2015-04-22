a76795683f6842ca8e0495186d66c09bfd834703
jbarney86 <jefferydale1986@gmail.com>
2015-04-21 21:15:11 -0600

    Update loaders.js

--- src/loaders.js
+++ src/loaders.js
@@ -1,16 +1,16 @@
 
 var scripts = [
-    "https://rawgit.com/jbarney86/BotExtension/master/commands.js",
-    "https://rawgit.com/jbarney86/BotExtension/master/cron.js",
-    "https://rawgit.com/jbarney86/BotExtension/master/lottery.js",
-    "https://rawgit.com/jbarney86/BotExtension/master/blackjack.js",
-    "https://rawgit.com/jbarney86/BotExtension/master/afkcheck.js",
-    "https://rawgit.com/jbarney86/BotExtension/master/handlers.js",
-    "https://rawgit.com/jbarney86/BotExtension/master/bot.js"];
+    "https://rawgit.com/jbarney86/BotExtension/master/src/commands.js",
+    "https://rawgit.com/jbarney86/BotExtension/master/src/cron.js",
+    "https://rawgit.com/jbarney86/BotExtension/master/src/lottery.js",
+    "https://rawgit.com/jbarney86/BotExtension/master/src/blackjack.js",
+    "https://rawgit.com/jbarney86/BotExtension/master/src/afkcheck.js",
+    "https://rawgit.com/jbarney86/BotExtension/master/src/handlers.js",
+    "https://rawgit.com/jbarney86/BotExtension/master/src/bot.js"];
 
 
 function updateBot() {
-    setTimeout(function(){$.getScript("https://rawgit.com/jbarney86/BotExtension/master/loader.js");}, 2000);
+    setTimeout(function(){$.getScript("https://rawgit.com/jbarney86/BotExtension/master/src/loaders.js");}, 2000);
 }
 
 
